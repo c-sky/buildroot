480b81f4136ad29dab1c4bb266c122aa8d44e873
Guo Ren <ren_guo@c-sky.com>
2017-03-03 11:02:52 +0800

    linux: add dummy addons for linux csky
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- linux/linux-ext-csky-arch.mk
+++ linux/linux-ext-csky-arch.mk
@@ -8,5 +8,10 @@ LINUX_EXTENSIONS += csky-arch
 
 define CSKY_ARCH_PREPARE_KERNEL
 	cp -raf $(CSKY_ARCH_DIR)/arch/csky $(LINUX_DIR)/arch/
+	if [ !$(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS) ]; then \
+		mkdir $(LINUX_DIR)/addons; \
+		touch $(LINUX_DIR)/addons/Kconfig; \
+		touch $(LINUX_DIR)/addons/Makefile; \
+	fi
 endef
 
