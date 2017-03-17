e4f427df08bfd18b724c62fc1797f047c530728b
Guo Ren <ren_guo@c-sky.com>
2017-03-17 14:32:05 +0800

    linux: update linux-ext-csky-addons with new dir
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- linux/linux-ext-csky-addons.mk
+++ linux/linux-ext-csky-addons.mk
@@ -7,6 +7,10 @@
 LINUX_EXTENSIONS += csky-addons
 
 define CSKY_ADDONS_PREPARE_KERNEL
-	cp -raf $(CSKY_ADDONS_DIR) $(LINUX_DIR)/addons
+	if [ -f $(CSKY_ADDONS_DIR)/addons/Kconfig ]; then \
+		cp -raf $(CSKY_ADDONS_DIR)/addons $(LINUX_DIR)/addons; \
+	else \
+		cp -raf $(CSKY_ADDONS_DIR) $(LINUX_DIR)/addons; \
+	fi
 endef
 
