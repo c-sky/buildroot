054eee733cb31d46d239d92dfe92d09a5767438a
Guo Ren <ren_guo@c-sky.com>
2017-05-19 20:52:53 +0800

    package: add aliyun-iot-device-sdk.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

---
+++ package/aliyun-iot-device-sdk/aliyun-iot-device-sdk.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# aliyun-iot-device-sdk
+#
+################################################################################
+
+ALIYUN_IOT_DEVICE_SDK_VERSION = c-2017-05-11
+ALIYUN_IOT_DEVICE_SDK_SOURCE = aliyun-iot-device-sdk-$(ALIYUN_IOT_DEVICE_SDK_VERSION).zip
+ALIYUN_IOT_DEVICE_SDK_SITE = http://aliyun-iot.oss-cn-hangzhou.aliyuncs.com
+ALIYUN_IOT_DEVICE_SDK_LICENSE = EPL-1.0 or EDLv1.0
+
+define ALIYUN_IOT_DEVICE_SDK_EXTRACT_CMDS
+	$(UNZIP) -d $(@D) $(DL_DIR)/$(ALIYUN_IOT_DEVICE_SDK_SOURCE)
+	mv $(@D)/aliyun-iot-device-sdk-$(ALIYUN_IOT_DEVICE_SDK_VERSION)/* $(@D)
+	$(RM) -r $(@D)/aliyun-iot-device-sdk-$(ALIYUN_IOT_DEVICE_SDK_VERSION)
+	echo "PLATFORM_CC = $(TARGET_CC)" >> $(@D)/make.settings
+	echo "PLATFORM_AR = $(TARGET_AR)" >> $(@D)/make.settings
+endef
+
+define ALIYUN_IOT_DEVICE_SDK_CONFIGURE_CMDS
+endef
+
+define ALIYUN_IOT_DEVICE_SDK_BUILD_CMDS
+	cd $(@D); make; cd -
+endef
+
+define ALIYUN_IOT_DEVICE_SDK_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/aliyun-iot-device-sdk/examples/linux/mqtt/
+	cp $(@D)/examples/linux/mqtt/demo $(TARGET_DIR)/aliyun-iot-device-sdk/examples/linux/mqtt/
+endef
+
+# It's not autotools-based
+$(eval $(generic-package))
+$(eval $(host-generic-package))
