4aa044b859770671a08521699a3761ef7511e1ad
Guo Ren <ren_guo@c-sky.com>
2017-05-24 10:36:57 +0800

    package: change the csky-debug path for host install.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/csky-debug/csky-debug.mk
+++ package/csky-debug/csky-debug.mk
@@ -9,7 +9,8 @@ CSKY_DEBUG_SOURCE = DebugServerConsole-linux-x86_64-$(CSKY_DEBUG_VERSION).tar.gz
 CSKY_DEBUG_SITE = https://github.com/c-sky/tools/raw/master
 
 define HOST_CSKY_DEBUG_INSTALL_CMDS
-	cp $(@D)/* $(HOST_DIR)/usr/bin/
+	mkdir -p $(HOST_DIR)/csky-debug
+	cp -raf $(@D)/* $(HOST_DIR)/csky-debug
 endef
 
 $(eval $(host-generic-package))
