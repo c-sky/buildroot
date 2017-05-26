51ccf90204357eec982bf6f6432b5e78c64ae067
Guo Ren <ren_guo@c-sky.com>
2017-05-26 15:10:00 +0800

    toolchain: toolchain-external-csky added with glibc.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

---
+++ toolchain/toolchain-external/toolchain-external-csky/toolchain-external-csky.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# toolchain-external-csky
+#
+################################################################################
+
+TOOLCHAIN_EXTERNAL_CSKY_SITE = https://github.com/c-sky/tools/raw/master
+
+ifeq ($(BR2_TOOLCHAIN_EXTERNAL_CSKY_GLIBC),y)
+ifeq ($(BR2_ck610),y)
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-glibc-linux-4.9.25-20170522.tar.gz
+else
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-glibc-linux-4.9.25-20170522.tar.gz
+endif
+else # BR2_TOOLCHAIN_EXTERNAL_CSKY_GLIBC
+ifeq ($(BR2_ck610),y)
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-uclibc-linux-4.9.25-20170521.tar.gz
+else
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-uclibc-linux-4.9.25-20170521.tar.gz
+endif
+endif # BR2_TOOLCHAIN_EXTERNAL_CSKY_GLIBC
+
+$(eval $(toolchain-external-package))
