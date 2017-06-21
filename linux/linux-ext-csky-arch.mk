8ac348523cfe35cd7af01ac65d834e6066f061ab
Guo Ren <ren_guo@c-sky.com>
2017-06-21 18:18:28 +0800

    linux: BUGFIX linux-ext-csky-arch prepare none addons.
    Change-Id: I591f4a9ea2c9ccda452a54a4d575b3f0e8e81a21
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- linux/linux-ext-csky-arch.mk
+++ linux/linux-ext-csky-arch.mk
@@ -6,13 +6,16 @@
 
 LINUX_EXTENSIONS += csky-arch
 
+ifeq ($(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS),y)
 define CSKY_ARCH_PREPARE_KERNEL
 	cp -raf $(CSKY_ARCH_DIR)/arch/csky $(LINUX_DIR)/arch/
-	if [ !$(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS) ]; then \
-		mkdir $(LINUX_DIR)/addons; \
-		touch $(LINUX_DIR)/addons/Kconfig; \
-		touch $(LINUX_DIR)/addons/none.c; \
-		echo "obj-y += none.o" > $(LINUX_DIR)/addons/Makefile; \
-	fi
 endef
-
+else
+define CSKY_ARCH_PREPARE_KERNEL
+	cp -raf $(CSKY_ARCH_DIR)/arch/csky $(LINUX_DIR)/arch/
+	mkdir $(LINUX_DIR)/addons
+	touch $(LINUX_DIR)/addons/Kconfig
+	touch $(LINUX_DIR)/addons/none.c
+	echo "obj-y += none.o" > $(LINUX_DIR)/addons/Makefile
+endef
+endif
