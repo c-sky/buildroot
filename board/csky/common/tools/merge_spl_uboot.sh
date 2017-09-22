6ade4da7e839cb482c3e0eeacaa1260d0b8b019b
Lu ChongZhi <chongzhi_lu@c-sky.com>
2017-09-22 21:40:00 +0800

    Add '-f' after rm to avoid error when there is no such file(s) to delete.
    Change-Id: I9044f89b01c83394a8635b5ca9a60bc6c0e0a9c6

--- board/csky/common/tools/merge_spl_uboot.sh
+++ board/csky/common/tools/merge_spl_uboot.sh
@@ -3,5 +3,5 @@ dd if=/dev/zero of=./temp.bin bs=512 count=24
 cat ./output/images/u-boot-spl.bin ./temp.bin >> temp_a.bin
 dd if=./temp_a.bin of=./temp_b.bin bs=512 count=24
 cat ./temp_b.bin ./output/images/u-boot.bin >> ./output/images/spl_uboot.bin
-rm ./temp.bin ./temp_a.bin ./temp_b.bin ./output/images/u-boot-spl.bin ./output/images/u-boot.bin
+rm -f ./temp.bin ./temp_a.bin ./temp_b.bin ./output/images/u-boot-spl.bin ./output/images/u-boot.bin
 
