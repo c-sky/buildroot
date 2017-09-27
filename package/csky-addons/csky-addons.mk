9ff8e55d0f3b38a3016aff1775d47ff9906ebaa3
Guo Ren <ren_guo@c-sky.com>
2017-09-27 21:55:18 +0800

    csky: update csky-arch verion to center control.
    Change-Id: I8be367ae2f97929c9189e1917ae56c32688b4cb1
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/csky-addons/csky-addons.mk
+++ package/csky-addons/csky-addons.mk
@@ -4,7 +4,12 @@
 #
 ################################################################################
 
+CSKY_ADDONS_VERSION = 08c088a04e58f3dcdb4182e286c35401db876915
+
+ifneq ($(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_VERSION), "")
 CSKY_ADDONS_VERSION = $(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_VERSION)
+endif
+
 ifeq ($(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_GITHUB),y)
 CSKY_ADDONS_SITE = $(call github,c-sky,addons-linux,$(CSKY_ADDONS_VERSION))
 else
--- package/csky-arch/csky-arch.mk
+++ package/csky-arch/csky-arch.mk
@@ -4,7 +4,12 @@
 #
 ################################################################################
 
+CSKY_ARCH_VERSION = ab3f0cc0b819202d98543f6cf8e5ffd4c77d45b9
+
+ifneq ($(BR2_LINUX_KERNEL_EXT_CSKY_ARCH_VERSION), "")
 CSKY_ARCH_VERSION = $(BR2_LINUX_KERNEL_EXT_CSKY_ARCH_VERSION)
+endif
+
 ifeq ($(BR2_LINUX_KERNEL_EXT_CSKY_ARCH_GITHUB),y)
 CSKY_ARCH_SITE = $(call github,c-sky,csky-linux,$(CSKY_ARCH_VERSION))
 else
