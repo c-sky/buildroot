c552f612f83cdae6cce125d01b2b86081407d14e
Guo Ren <ren_guo@c-sky.com>
2017-10-12 10:51:08 +0800

    toolchain: update csky toolchain to gcc-6.3 and glibc-2.25
    Change-Id: I123111285740a2ed201c6121c4e3b1d4b5bfadb2
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- toolchain/toolchain-external/toolchain-external-csky/toolchain-external-csky.mk
+++ toolchain/toolchain-external/toolchain-external-csky/toolchain-external-csky.mk
@@ -8,15 +8,15 @@ TOOLCHAIN_EXTERNAL_CSKY_SITE = https://github.com/c-sky/tools/raw/master
 
 ifeq ($(BR2_TOOLCHAIN_EXTERNAL_CSKY_GLIBC),y)
 ifeq ($(BR2_ck610),y)
-TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-glibc-linux-4.9.25-20170522.tar.gz
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-glibc-linux-4.9.25-20170929.tar.gz
 else
-TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-glibc-linux-4.9.25-20170825.tar.gz
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-glibc-linux-4.9.25-20170929.tar.gz
 endif
 else # BR2_TOOLCHAIN_EXTERNAL_CSKY_GLIBC
 ifeq ($(BR2_ck610),y)
-TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-uclibc-linux-4.9.25-20170927.tar.gz
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-uclibc-linux-4.9.25-20170930.tar.gz
 else
-TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-uclibc-linux-4.9.25-20170927.tar.gz
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-uclibc-linux-4.9.25-20170930.tar.gz
 endif
 endif # BR2_TOOLCHAIN_EXTERNAL_CSKY_GLIBC
 
