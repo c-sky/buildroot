6807b8ca231030fb45d166c5b389a44b54ec57b8
Guo Ren <ren_guo@c-sky.com>
2017-11-01 16:05:51 +0800

    csky: compile csky-qemu in buildroot.
    Change-Id: If389da2ca8430bf19ca6dabb854e6df23145663f
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/csky-qemu/csky-qemu.mk
+++ package/csky-qemu/csky-qemu.mk
@@ -4,13 +4,26 @@
 #
 ################################################################################
 
-CSKY_QEMU_VERSION = 20171010
-CSKY_QEMU_SOURCE = csky-qemu-x86_64-$(CSKY_QEMU_VERSION).tar.gz
-CSKY_QEMU_SITE = https://github.com/c-sky/tools/raw/master
+CSKY_QEMU_VERSION = ce825f67bca4bbda1cd091e1bf6511fddaddb1a3
+CSKY_QEMU_SITE = $(call github,c-sky,qemu,$(CSKY_QEMU_VERSION))
+
+HOST_QEMU_DEPENDENCIES = host-pkgconf host-python host-zlib host-libglib2 host-pixman
+
+define HOST_CSKY_QEMU_CONFIGURE_CMDS
+		cd $(@D); \
+		./configure \
+		--target-list="cskyv1-softmmu cskyv2-softmmu" \
+		--prefix="$(HOST_DIR)/csky-qemu"
+endef
+
+define HOST_CSKY_QEMU_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)
+endef
 
 define HOST_CSKY_QEMU_INSTALL_CMDS
-	mkdir -p $(HOST_DIR)/csky-qemu
-	cp -raf $(@D)/* $(HOST_DIR)/csky-qemu
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) install
+	cp $(@D)/cskyv1-softmmu/qemu-system-cskyv1 $(HOST_DIR)/csky-qemu/bin
+	cp $(@D)/cskyv2-softmmu/qemu-system-cskyv2 $(HOST_DIR)/csky-qemu/bin
 endef
 
 $(eval $(host-generic-package))
