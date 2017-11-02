9faa542952de8bdf2b5f520128f4871fcd497156
Guo Ren <ren_guo@c-sky.com>
2017-11-02 10:31:56 +0800

    package/csky-qemu: fix compile error on gitlab.
    Change-Id: I7413bd5a350ca14a345ee2500186072557544f9e
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/csky-qemu/csky-qemu.mk
+++ package/csky-qemu/csky-qemu.mk
@@ -7,13 +7,22 @@
 CSKY_QEMU_VERSION = ce825f67bca4bbda1cd091e1bf6511fddaddb1a3
 CSKY_QEMU_SITE = $(call github,c-sky,qemu,$(CSKY_QEMU_VERSION))
 
-HOST_QEMU_DEPENDENCIES = host-pkgconf host-python host-zlib host-libglib2 host-pixman
+HOST_CSKY_QEMU_DEPENDENCIES = host-pkgconf host-python host-zlib host-libglib2 host-pixman
+
+HOST_CSKY_QEMU_OPTS += --enable-system --enable-fdt
 
 define HOST_CSKY_QEMU_CONFIGURE_CMDS
-		cd $(@D); \
-		./configure \
-		--target-list="cskyv1-softmmu cskyv2-softmmu" \
-		--prefix="$(HOST_DIR)/csky-qemu"
+	cd $(@D); $(HOST_CONFIGURE_OPTS) CPP="$(HOSTCC) -E" \
+	./configure \
+	--target-list="cskyv1-softmmu cskyv2-softmmu" \
+	--prefix="$(HOST_DIR)/csky-qemu" \
+	--interp-prefix=$(STAGING_DIR) \
+	--cc="$(HOSTCC)" \
+	--host-cc="$(HOSTCC)" \
+	--python=$(HOST_DIR)/bin/python2 \
+	--extra-cflags="$(HOST_CFLAGS)" \
+	--extra-ldflags="$(HOST_LDFLAGS)" \
+	$(HOST_CSKY_QEMU_OPTS)
 endef
 
 define HOST_CSKY_QEMU_BUILD_CMDS
