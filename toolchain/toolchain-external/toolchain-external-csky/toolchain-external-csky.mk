321d9140b7ccd2b125690b6775d0e4410f5f309a
Guo Ren <ren_guo@c-sky.com>
2017-12-05 19:40:24 +0800

    csky: update toolchain to correct the gdb.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- toolchain/toolchain-external/toolchain-external-csky/toolchain-external-csky.mk
+++ toolchain/toolchain-external/toolchain-external-csky/toolchain-external-csky.mk
@@ -8,15 +8,15 @@ TOOLCHAIN_EXTERNAL_CSKY_SITE = https://github.com/c-sky/tools/raw/master
 
 ifeq ($(BR2_TOOLCHAIN_EXTERNAL_CSKY_GLIBC),y)
 ifeq ($(BR2_ck610),y)
-TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-glibc-linux-4.9.56-20171030.tar.gz
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-glibc-linux-4.9.56-20171205.tar.gz
 else
-TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-glibc-linux-4.9.56-20171030.tar.gz
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-glibc-linux-4.9.56-20171205.tar.gz
 endif
 else # BR2_TOOLCHAIN_EXTERNAL_CSKY_GLIBC
 ifeq ($(BR2_ck610),y)
-TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-uclibc-linux-4.9.56-20171030.tar.gz
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-linux-tools-x86_64-uclibc-linux-4.9.56-20171205.tar.gz
 else
-TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-uclibc-linux-4.9.56-20171030.tar.gz
+TOOLCHAIN_EXTERNAL_CSKY_SOURCE = csky-abiv2-linux-tools-x86_64-uclibc-linux-4.9.56-20171205.tar.gz
 endif
 endif # BR2_TOOLCHAIN_EXTERNAL_CSKY_GLIBC
 
