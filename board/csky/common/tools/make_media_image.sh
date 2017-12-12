185348de31a19f23b825c0b6303dabe8a8afc2d9
Lu ChongZhi <chongzhi_lu@c-sky.com>
2017-12-12 19:14:02 +0800

    Modify from /bin/sh to be /bin/bash; Add compatible code for sh/bash
    Change-Id: I0eaa3abff9b1985eaa846c6e11fc4191f4afeeff

--- board/csky/common/tools/make_media_image.sh
+++ board/csky/common/tools/make_media_image.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 BINARIES_DIR=$1
 
 media_files_path="${BINARIES_DIR}/../build/csky-examples-*/media/"
 media_img_name="${BINARIES_DIR}/media-partition.ext2"
 
-if [ -d $media_files_path ]; then
+if [[ -d $media_files_path ]]; then
 	rm -f $media_img_name
 	# The partition is 128MB (512*262144)
 	genext2fs -b 131072 -d $media_files_path $media_img_name
