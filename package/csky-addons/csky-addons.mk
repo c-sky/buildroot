c33faa1b3d83f2bcc88d4a185ebb27f9eee3a3d6
Guo Ren <ren_guo@c-sky.com>
2018-01-18 17:12:09 +0800

    package/csky-addons: new silan sc8925 dev board support.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/csky-addons/csky-addons.mk
+++ package/csky-addons/csky-addons.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CSKY_ADDONS_VERSION = 08c088a04e58f3dcdb4182e286c35401db876915
+CSKY_ADDONS_VERSION = 972d0d7bb62f56e967e2afdca1cabee38b3ec986
 
 ifneq ($(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_VERSION), "")
 CSKY_ADDONS_VERSION = $(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_VERSION)
