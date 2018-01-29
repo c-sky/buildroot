cae433bbda903e2b067622187b5e4eb15f6ded1d
Mao Han <han_mao@c-sky.com>
2018-01-29 17:54:10 +0800

    csky:fix csky-addons compile warning as Werror is set

--- package/csky-addons/csky-addons.mk
+++ package/csky-addons/csky-addons.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CSKY_ADDONS_VERSION = 972d0d7bb62f56e967e2afdca1cabee38b3ec986
+CSKY_ADDONS_VERSION = c293aaf9ee74baa2d1c8247255a515c449af70c1
 
 ifneq ($(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_VERSION), "")
 CSKY_ADDONS_VERSION = $(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_VERSION)
