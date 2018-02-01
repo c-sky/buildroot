c17789f49d4b92418c1f3eb88a39d1083607b6e8
Mao Han <han_mao@c-sky.com>
2018-02-01 15:23:06 +0800

    csky test:move some makefile script to csky test

--- package/csky-test/csky-test.mk
+++ package/csky-test/csky-test.mk
@@ -5,7 +5,7 @@
 #
 ################################################################################
 
-CSKY_TEST_VERSION = fcfbe32d81df53f917bfbf2f5d50a1d2e0780f81
+CSKY_TEST_VERSION = 9a65a370c8e613d4e04409c4b1177f6982a063d5
 CSKY_TEST_SITE = $(call github,riseandfall,csky-test,$(CSKY_TEST_VERSION))
 ifeq ($(BR2_CSKY_TEST_GDB_FILE),)
 CSKY_TEST_CP_GDBINIT =
@@ -18,6 +18,7 @@ echo CONFIG_CPU=$(BR2_CSKY_TEST_CPU) > $(@D)/config
 echo CONFIG_LIBC=$(BR2_CSKY_TEST_LIBC) >> $(@D)/config
 echo CONFIG_QEMU=$(BR2_CSKY_TEST_QEMU) >> $(@D)/config
 echo CONFIG_GDB=$(BR2_CSKY_TEST_GDB_FILE) >> $(@D)/config
+echo CONFIG_TTY=$(BR2_TARGET_GENERIC_GETTY_PORT) >> $(@D)/config
 echo CONFIG_NFS=$(BR2_CSKY_TEST_NFS_PATH) >> $(@D)/config
 echo CONFIG_LTP=$(BR2_PACKAGE_CSKY_TEST_LTP) >> $(@D)/config
 echo CONFIG_LMBENCH=$(BR2_PACKAGE_CSKY_TEST_LMBENCH) >> $(@D)/config
@@ -29,7 +30,6 @@ define CSKY_TEST_BUILD_CMDS
 make -C $(@D)
 endef
 
-
 define CSKY_TEST_INSTALL_TARGET_CMDS
 mkdir -p $(HOST_DIR)/csky-test/
 mkdir -p $(TARGET_DIR)/usr/lib/csky-test/
@@ -38,6 +38,30 @@ cp -f $(@D)/out/configs/* $(TARGET_DIR)/usr/lib/csky-test/
 cp -f $(@D)/out/S90test $(TARGET_DIR)/etc/init.d/
 $(CSKY_TEST_CP_GDBINIT)
 endef
+
+ifeq ($(BR2_TOOLCHAIN_BUILDROOT),y)
+define  HOST_GCC_FINAL_CSKY_TARBALL
+        (cd $(HOST_DIR); \
+         BUILDROOT_VERSION=$$(git log --pretty=oneline|head -1|awk '{print $$1}'); \
+         BUILDROOT_CONFIG=$$(grep BR2_DEFCONFIG $(CONFIG_DIR)/.config|awk -F/ '{print $$NF}'|sed 's/\"//g'); \
+         echo $$BUILDROOT_VERSION; \
+         echo $$BUILDROOT_CONFIG; \
+         tar -czf $(BINARIES_DIR)/csky_toolchain_$${BUILDROOT_CONFIG}_$${BUILDROOT_VERSION}.tar.gz bin csky-buildroot* include lib lib64 libexec share usr; \
+         cd - ;\
+        )
+endef
+HOST_GCC_FINAL_POST_INSTALL_HOOKS += HOST_GCC_FINAL_CSKY_TARBALL
+endif
+
+ifeq ($(BR2_PACKAGE_LMBENCH),y)
+define  LMBENCH_CP_SCRIPT
+        (\
+         cp $(@D)/bin/csky/lmbench $(TARGET_DIR)/usr/bin/;\
+         echo cp $(@D)/bin/csky/lmbench $(TARGET_DIR)/usr/bin/;\
+        )
+endef
+LMBENCH_POST_INSTALL_TARGET_HOOKS += LMBENCH_CP_SCRIPT
+endif
 
 $(eval $(generic-package))
 $(eval $(host-generic-package))
