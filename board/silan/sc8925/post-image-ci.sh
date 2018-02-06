98ffdab2fa562f302f7fe67651e25a1f66c4df7d
Mao Han <han_mao@c-sky.com>
2018-02-06 17:28:43 +0800

    csky ci:rearrage folder structure

---
+++ board/silan/sc8925/post-image-ci.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp ./board/silan/sc8925/gdbinit_nfs $(BINARIES_DIR)
+
---
+++ package/csky-ci/csky-ci.mk
@@ -0,0 +1,53 @@
+#
+##############################################################################
+#
+# csky ci
+#
+################################################################################
+
+CSKY_CI_VERSION = 8af57677d955eabace81667010dc3dc408db3179
+CSKY_CI_SITE = $(call github,riseandfall,csky-ci,$(CSKY_CI_VERSION))
+
+define CSKY_CI_CONFIGURE_CMDS
+echo CONFIG_CPU_CK610=$(BR2_ck610) > $(@D)/config
+echo CONFIG_GLIBC=$(BR2_TOOLCHAIN_USES_GLIBC) >> $(@D)/config
+echo CONFIG_QEMU=$(BR2_PACKAGE_HOST_CSKY_QEMU) >> $(@D)/config
+echo CONFIG_TTY=$(BR2_TARGET_GENERIC_GETTY_PORT) >> $(@D)/config
+endef
+
+define CSKY_CI_BUILD_CMDS
+make -C $(@D)
+endef
+
+define CSKY_CI_INSTALL_TARGET_CMDS
+mkdir -p $(HOST_DIR)/csky-ci/
+mkdir -p $(TARGET_DIR)/usr/lib/csky-ci/
+cp -f $(@D)/out/sh/* $(HOST_DIR)/csky-ci/
+cp -f $(@D)/out/configs/* $(TARGET_DIR)/usr/lib/csky-ci/
+cp -f $(@D)/out/S90test $(TARGET_DIR)/etc/init.d/
+endef
+
+ifeq ($(BR2_TOOLCHAIN_BUILDROOT),y)
+define  HOST_GCC_FINAL_CSKY_TARBALL
+        (cd $(HOST_DIR); \
+         BUILDROOT_VERSION=$$(git log --pretty=oneline|head -1|awk '{print $$1}'); \
+         BUILDROOT_CONFIG=$$(grep BR2_DEFCONFIG $(CONFIG_DIR)/.config|awk -F/ '{print $$NF}'|sed 's/\"//g'); \
+         echo $$BUILDROOT_CONFIG > bin/.csky_bt_commit; \
+         echo $$BUILDROOT_VERSION >> bin/.csky_bt_commit; \
+         tar -czf $(BINARIES_DIR)/csky_toolchain_$${BUILDROOT_CONFIG}_$${BUILDROOT_VERSION}.tar.gz bin csky-buildroot* include lib lib64 libexec share usr; \
+         cd - ;\
+        )
+endef
+HOST_GCC_FINAL_POST_INSTALL_HOOKS += HOST_GCC_FINAL_CSKY_TARBALL
+endif
+
+ifeq ($(BR2_PACKAGE_LMBENCH),y)
+define  LMBENCH_CP_SCRIPT
+        (\
+         cp $(@D)/bin/csky/lmbench $(TARGET_DIR)/usr/bin/;\
+        )
+endef
+LMBENCH_POST_INSTALL_TARGET_HOOKS += LMBENCH_CP_SCRIPT
+endif
+
+$(eval $(generic-package))
