73e947e16191094aad413886dd7b09905c8838cc
Guo Ren <ren_guo@c-sky.com>
2018-03-06 11:09:35 +0800

    linux-ext-csky: don't modify arch/csky, patch to linux.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- linux/linux-ext-csky-addons.mk
+++ linux/linux-ext-csky-addons.mk
@@ -12,5 +12,8 @@ define CSKY_ADDONS_PREPARE_KERNEL
 	else \
 		cp -raf $(CSKY_ADDONS_DIR) $(LINUX_DIR)/addons; \
 	fi
+	awk '/:= drivers/{print $$0,"addons/";next}{print $$0}'\
+		$(LINUX_DIR)/Makefile 1<>$(LINUX_DIR)/Makefile
+	echo "source \"addons/Kconfig\"" >> $(LINUX_DIR)/lib/Kconfig
 endef
 
--- linux/linux-ext-csky-arch.mk
+++ linux/linux-ext-csky-arch.mk
@@ -6,14 +6,6 @@
 
 LINUX_EXTENSIONS += csky-arch
 
-ifeq ($(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS),y)
-define CSKY_ARCH_PREPARE_KERNEL
-	cp -raf $(CSKY_ARCH_DIR)/arch/csky $(LINUX_DIR)/arch/
-	echo "drivers-y += addons/" >> $(LINUX_DIR)/arch/csky/Makefile
-	echo "source \"addons/Kconfig\"" >> $(LINUX_DIR)/arch/csky/Kconfig
-endef
-else
 define CSKY_ARCH_PREPARE_KERNEL
 	cp -raf $(CSKY_ARCH_DIR)/arch/csky $(LINUX_DIR)/arch/
 endef
-endif
