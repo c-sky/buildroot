65b307936d5b2ace96a66ef42728892eee9de226
Guo Ren <ren_guo@c-sky.com>
2018-03-08 08:32:58 +0800

    linux/csky: patch for linux, make arch/csky clean.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- linux/linux-ext-csky-arch.mk
+++ linux/linux-ext-csky-arch.mk
@@ -8,4 +8,7 @@ LINUX_EXTENSIONS += csky-arch
 
 define CSKY_ARCH_PREPARE_KERNEL
 	cp -raf $(CSKY_ARCH_DIR)/arch/csky $(LINUX_DIR)/arch/
+	cp -raf $(CSKY_ARCH_DIR)/arch-csky-drivers $(LINUX_DIR)/
+	awk '/:= drivers/{print $$0,"arch-csky-drivers/";next}{print $$0}' \
+		$(LINUX_DIR)/Makefile 1<>$(LINUX_DIR)/Makefile
 endef
