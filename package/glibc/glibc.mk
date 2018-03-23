0a6baa08377b8eec80eeb6519bdbab2c5e02c8dc
Mao Han <han_mao@c-sky.com>
2018-03-23 07:05:22 +0000

    Update glibc.mk

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -8,7 +8,7 @@ ifeq ($(BR2_arc),y)
 GLIBC_VERSION =  arc-2017.09-release
 GLIBC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,glibc,$(GLIBC_VERSION))
 else ifeq ($(BR2_csky),y)
-GLIBC_VERSION = 9b11561ce42350d663fda2bbb2de1530efc5364a
+GLIBC_VERSION = 8b33a3558b5f27cd9eb47b8c1a10b7efd52fd6b5
 GLIBC_SITE = $(call github,c-sky,glibc,$(GLIBC_VERSION))
 else
 # Generate version string using:
