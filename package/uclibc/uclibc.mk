9093883e04b988e5c998bc3dcf6881c68ef991aa
Guo Ren <ren_guo@c-sky.com>
2018-04-12 14:19:50 +0800

    csky: update uclibc version.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/uclibc/uclibc.mk
+++ package/uclibc/uclibc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifeq ($(BR2_csky),y)
-UCLIBC_VERSION = 96129bf0f9192330731a566fdde36d078a0c4b78
+UCLIBC_VERSION = cb0521fa4dc077263961ad2de39bf1d6d5ba0aef
 UCLIBC_SOURCE = uClibc-ng-$(UCLIBC_VERSION).tar.gz
 UCLIBC_SITE = $(call github,c-sky,uclibc-ng,$(UCLIBC_VERSION))
 UCLIBC_LICENSE = LGPL-2.1+
