ad288b6bfcfb90224b70e7a1c0e8b6f8e91f8f63
zhangmf <minfeng_zhang@c-sky.com>
2018-04-26 15:32:47 +0800

    update tools for csky sdk
    Change-Id: I2e6497c0b4b218926a994a928e677f0d4e16f221

--- board/csky/common/tools/make_media_image.sh
+++ board/csky/common/tools/make_media_image.sh
@@ -2,8 +2,9 @@
 
 BINARIES_DIR=$1
 
-media_files_path="${BINARIES_DIR}/../build/csky-examples-*/media/"
 media_img_name="${BINARIES_DIR}/media-partition.ext2"
+example_name=`(ls -f ${BINARIES_DIR}/../build/ | grep "csky-examples-")`
+media_files_path="${BINARIES_DIR}/../build/$example_name/media"
 
 if [[ -d $media_files_path ]]; then
 	rm -f $media_img_name
