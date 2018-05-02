b4887888377a8ba013f38c53407c54d3dde365eb
Guo Ren <ren_guo@c-sky.com>
2018-05-02 11:26:14 +0800

    csky: bugfix bash -f to determine the file existed.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/csky-arch/csky-arch.mk
+++ package/csky-arch/csky-arch.mk
@@ -31,7 +31,7 @@ endef
 CSKY_ARCH_POST_EXTRACT_HOOKS += CSKY_ARCH_VERSION_ADD
 
 define CSKY_LINUX_GENERATE_PATCH
-	if [ ! -z $(LINUX_DIR)/.stamp_patched_csky ]; then \
+	if [ ! -f $(LINUX_DIR)/.stamp_patched_csky ]; then \
 	cd $(LINUX_DIR)/../; \
 	mv linux-$(LINUX_VERSION) b; \
 	rm $(BINARIES_DIR)/linux-$(LINUX_VERSION).patch.xz; \
@@ -46,9 +46,11 @@ endef
 LINUX_POST_CONFIGURE_HOOKS += CSKY_LINUX_GENERATE_PATCH
 
 define CSKY_LINUX_PREPARE_SRC_A
+	if [ ! -f $(LINUX_DIR)/.stamp_patched_csky ]; then \
 	cd $(LINUX_DIR)/../; \
 	cp -raf linux-$(LINUX_VERSION) a; \
-	cd -
+	cd -; \
+	fi
 endef
 LINUX_POST_EXTRACT_HOOKS += CSKY_LINUX_PREPARE_SRC_A
 
