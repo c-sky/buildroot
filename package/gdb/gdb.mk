d9ee1202041e4df2909f45e5f75213e0e6431697
Mao Han <han_mao@c-sky.com>
2018-05-08 09:03:22 +0000

    csky gdb: update gdbserver

--- package/gdb/gdb.mk
+++ package/gdb/gdb.mk
@@ -54,7 +54,7 @@ HOST_GDB_DEPENDENCIES += host-flex host-bison
 endif
 
 ifeq ($(BR2_csky),y)
-GDB_VERSION = ce821ab28cc0f96fe4e9912b5e5a1259f67225e5
+GDB_VERSION = e1f6e1e6dcb62d1bcd7af9971630a2f2aad7564b
 GDB_SITE = $(call github,c-sky,binutils-gdb,$(GDB_VERSION))
 GDB_SOURCE = gdb-$(GDB_VERSION).tar.gz
 GDB_FROM_GIT = y
