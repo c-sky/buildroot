d3003b2e41e59e5ef1a3cd4546d1c98792e97347
chenhd <hongdeng_chen@c-sky.com>
2018-07-02 14:55:01 +0800

    TEST:add ck860f test defconfig
    Change-Id: Idc8d0a09ba51d2ad816be84802265b976279a50a

--- board/csky-qa/clean_lib.sh
+++ board/csky-qa/clean_lib.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 TOPDIR=`pwd`
+FS_OVERLAY_DIR="$TOPDIR/board/csky-qa/fs-overlay"
 TARGET_DIR=$TOPDIR/output/target/lib
 cd $TARGET_DIR
 for i in `ls -l |awk '/^d/ {print $NF}'`
@@ -9,3 +10,43 @@ do
 		rm -rf $i
 	fi
 done
+#******************
+# copy id_rsa.pub 
+#
+mkdir -p $FS_OVERLAY_DIR/root/.ssh
+cat /home/$USER/.ssh/id_rsa.pub > $FS_OVERLAY_DIR/root/.ssh/authorized_keys
+mkdir -p $FS_OVERLAY_DIR/home/$USER/.ssh
+cat /home/$USER/.ssh/id_rsa.pub > $FS_OVERLAY_DIR/home/$USER/.ssh/authorized_keys
+
+#*******************
+# creat S70sshd
+#
+uid=`id -u`
+gid=`id -g`
+user="$USER"
+group=`id|awk '{print $2}'|awk -F '(' '{print $2}'|awk -F ')' '{print $1}'`
+
+echo "$user $uid $group $gid"
+[ ! -d $FS_OVERLAY_DIR/etc/init.d/ ] && mkdir -p $FS_OVERLAY_DIR/etc/init.d/
+echo "#!/bin/sh
+echo \"start sshd ......\"
+chown root:root -R /var/empty/
+chown root:root -R /root/
+chmod 755 -R /root
+chown root:root /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+chmod 755 /var/empty/
+
+addgroup -g $gid $group
+id $user > /dev/null
+[ \$? != 0 ] && adduser -D -u $uid -G $group $user
+#addgroup $user $group
+chown $user:$group -R /home/$user/
+#chown $user:$user /home/$user/.ssh/authorized_keys
+chmod 600 /home/$user/.ssh/authorized_keys
+echo \"sshd:x:74:74:Privilege-separated SSH:/var/empty/sshd:/sbin/nologin\" >>/etc/passwd
+#chmod 600 /usr/local/etc/*
+/usr/sbin/sshd
+echo \"Finishd sshd !\"
+" > $FS_OVERLAY_DIR/etc/init.d/S70sshd
+chmod +x $FS_OVERLAY_DIR/etc/init.d/S70sshd
