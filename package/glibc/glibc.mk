b201bffbc9c01720e8fad24585d23febb04f9155
Guo Ren <49706113@qq.com>
2018-07-27 08:09:17 +0000

    Revert "csky glibc: fix pointer guard relocation"
    This reverts commit 59bf7fe20cf2b6a810792ddaaf2877772157cc12

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -8,7 +8,7 @@ ifeq ($(BR2_arc),y)
 GLIBC_VERSION =  arc-2017.09-release
 GLIBC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,glibc,$(GLIBC_VERSION))
 else ifeq ($(BR2_csky),y)
-GLIBC_VERSION = 28feb954cb78a0357ebe241bdb0ed45800e7686c
+GLIBC_VERSION = 620a285b2e914c34adf8d0bcc0d7454c9ca57ac0
 ifeq ($(BR2_CSKY_GERRIT_REPO),y)
 GLIBC_SITE = ssh://${GITUSER}@192.168.0.78:29418/tools/glibc
 GLIBC_SITE_METHOD = git
