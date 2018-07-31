64802e653cd2e19b69d13a0b75cbed5b16fe05c5
Guo Ren <ren_guo@c-sky.com>
2018-07-31 08:12:31 +0800

    csky: bugfix support ADDONS_VENDOR_REPO.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/csky-addons/csky-addons.mk
+++ package/csky-addons/csky-addons.mk
@@ -11,10 +11,18 @@ CSKY_ADDONS_VERSION = $(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_VERSION)
 endif
 
 ifeq ($(BR2_CSKY_GERRIT_REPO),y)
+ifeq ($(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_USE_VENDOR_REPO),y)
+CSKY_ADDONS_SITE = ssh://${GITUSER}@192.168.0.78:29418/os/linux-csky-addons-vendors
+else
 CSKY_ADDONS_SITE = ssh://${GITUSER}@192.168.0.78:29418/os/linux-csky-addons
+endif
 CSKY_ADDONS_SITE_METHOD = git
 else
+ifeq ($(BR2_LINUX_KERNEL_EXT_CSKY_ADDONS_USE_VENDOR_REPO),y)
+CSKY_ADDONS_SITE = $(call github,c-sky,addons-linux,$(CSKY_ADDONS_VERSION))
+else
 CSKY_ADDONS_SITE = $(call github,c-sky,linux-csky-addons,$(CSKY_ADDONS_VERSION))
+endif
 endif
 
 ifeq ($(BR2_GERRIT),y)
