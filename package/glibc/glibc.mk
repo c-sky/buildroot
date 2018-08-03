bee7c90b612748f1ff2620ba93fc98f1e57dd756
Mao Han <han_mao@c-sky.com>
2018-08-03 03:02:00 +0000

    Update glibc.mk

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -8,7 +8,7 @@ ifeq ($(BR2_arc),y)
 GLIBC_VERSION =  arc-2017.09-release
 GLIBC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,glibc,$(GLIBC_VERSION))
 else ifeq ($(BR2_csky),y)
-GLIBC_VERSION = 7b39ecd7eead1925806f24e9e97fd57e0f3415e0
+GLIBC_VERSION = cf2da50a27c53845a150181c890fb10b358ad26c
 ifeq ($(BR2_CSKY_GERRIT_REPO),y)
 GLIBC_SITE = ssh://${GITUSER}@192.168.0.78:29418/tools/glibc
 GLIBC_SITE_METHOD = git
