67c1f842add537b47978cd79323f65693117b355
Lu ChongZhi <chongzhi_lu@c-sky.com>
2018-09-14 09:13:28 +0800

    Add Anole's NPU related Linux patches
    Change-Id: I7397cd7c5cfb437b0eb274170c7a14ee447c638c

--- board/csky/common/tools/softlink_ddr_init.sh
+++ board/csky/common/tools/softlink_ddr_init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# If softlink exists but invalid, then delete it first
+if [ -L ./ddr_init ] && [ ! -e ./ddr_init ]; then
+	rm -f ./ddr_init
+fi
+
 if [ ! -L ./ddr_init ]; then
 	ln -sf ${BASE_DIR}/build/linux-*/addons/ddr_init ./
 fi
