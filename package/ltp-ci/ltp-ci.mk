394c55f038b1d95cd86a9cd5507596db82d40c91
Liu Yibin <yibin_liu@c-sky.com>
2018-12-16 18:13:54 +0800

    Support ltp
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- package/ltp-ci/ltp-ci.mk
+++ package/ltp-ci/ltp-ci.mk
@@ -13,9 +13,11 @@ else ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),y)
 endif
 
 define LTP_CI_INSTALL_TARGET_CMDS
-	cp -f ./package/ltp-ci/ltp-ci $(TARGET_DIR)/etc/init.ci/
+	cp -f ./package/ltp-ci/ltp_run $(TARGET_DIR)/etc/init.ci/
+	chmod a+x $(TARGET_DIR)/etc/init.ci/ltp_run
 	cp -f ./package/ltp-ci/$(skplst) $(TARGET_DIR)/usr/lib/csky-ci/ltp-skiplist
-	chmod a+x $(TARGET_DIR)/etc/init.ci/ltp-ci
+	cp -f ./package/ltp-ci/ltp_parse $(HOST_DIR)/csky-ci/parse_script/
+	chmod a+x $(HOST_DIR)/csky-ci/parse_script/ltp_parse
 endef
 
 $(eval $(generic-package))
