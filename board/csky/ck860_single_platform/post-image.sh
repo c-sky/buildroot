5c53e25eda110346797079ab74f35916d89d3f35
Liu Yibin <yibin_liu@c-sky.com>
2018-12-20 09:44:01 +0800

    Remove all readme.txt related operations
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- board/csky/ck860_single_platform/post-image.sh
+++ board/csky/ck860_single_platform/post-image.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cp ./board/csky/ck860_single_platform/gdbinit ${BINARIES_DIR}
 cp ./board/csky/ck860_platform/ddr_init ${BINARIES_DIR}
-cp ./board/csky/ck860_platform/readme.txt ${BINARIES_DIR}
