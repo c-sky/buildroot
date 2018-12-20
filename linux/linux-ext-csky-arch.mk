2b245e5185faa3acca4255c320bea121d018a39d
Guo Ren <ren_guo@c-sky.com>
2018-12-20 10:40:26 +0800

    csky: linux kernel 4.19 supported
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- linux/linux-ext-csky-arch.mk
+++ linux/linux-ext-csky-arch.mk
@@ -8,7 +8,12 @@ LINUX_EXTENSIONS += csky-arch
 
 define CSKY_ARCH_PREPARE_KERNEL
 	cp -raf $(CSKY_ARCH_DIR)/arch/csky $(LINUX_DIR)/arch/
-	cp -raf $(CSKY_ARCH_DIR)/arch-csky-drivers $(LINUX_DIR)/
+	if [ -d $(CSKY_ARCH_DIR)/arch-csky-drivers ]; then \
+		cp -raf $(CSKY_ARCH_DIR)/arch-csky-drivers $(LINUX_DIR)/; \
+	fi
+	if [ -d $(CSKY_ARCH_DIR)/drivers ]; then \
+		cp -raf $(CSKY_ARCH_DIR)/drivers $(LINUX_DIR)/arch-csky-drivers; \
+	fi
 	awk '/:= drivers/{print $$0,"arch-csky-drivers/";next}{print $$0}' \
 		$(LINUX_DIR)/Makefile 1<>$(LINUX_DIR)/Makefile
 endef
