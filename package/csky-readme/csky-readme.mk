3068d8eab100c6f263d5366d9a6867fd188c1c71
Liu Yibin <yibin_liu@c-sky.com>
2018-12-23 11:24:13 +0800

    Fix readme log bug
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- package/csky-readme/csky-readme.mk
+++ package/csky-readme/csky-readme.mk
@@ -4,12 +4,15 @@
 #
 ##############################################################################
 
+BUILDROOT_VERSION=$(shell git log --pretty=oneline | head -1 | awk '{print $$1}')
+BUILDROOT_CONFIG=$(shell grep BR2_DEFCONFIG $(CONFIG_DIR)/.config | awk -F/ '{print $$NF}'| sed 's/\"//g')
+
 define CSKY_README_INSTALL_TARGET_CMDS
-	BUILDROOT_VERSION=$$(git log --pretty=oneline|head -1|awk '{print $$1}');
-	BUILDROOT_CONFIG=$$(grep BR2_DEFCONFIG $(CONFIG_DIR)/.config|awk -F/ '{print $$NF}'|sed 's/\"//g');
+	echo $(BUILDROOT_CONFIG)
+	echo $(BUILDROOT_VERSION)
 	cp package/csky-readme/readme.txt $(BINARIES_DIR)/
-	sed -i 's/<buildroot-config>/'$$BUILDROOT_CONFIG'/g' $(BINARIES_DIR)/readme.txt; \
-	sed -i 's/<buildroot-version>/'$$BUILDROOT_VERSION'/g' $(BINARIES_DIR)/readme.txt
+	sed -i 's/<buildroot-config>/$(BUILDROOT_CONFIG)/g' $(BINARIES_DIR)/readme.txt; \
+	sed -i 's/<buildroot-version>/$(BUILDROOT_VERSION)/g' $(BINARIES_DIR)/readme.txt
 	sed -i 's/<kernel-version>/$(LINUX_VERSION)/g' $(BINARIES_DIR)/readme.txt
 endef
 
