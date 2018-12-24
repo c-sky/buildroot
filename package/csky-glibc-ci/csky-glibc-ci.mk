ca99b3343e645136bd28ae968650acc758e86b6a
Liu Yibin <yibin_liu@c-sky.com>
2018-12-24 14:21:09 +0800

    csky-glibc-ci: use the correct tree
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- package/csky-glibc-ci/csky-glibc-ci.mk
+++ package/csky-glibc-ci/csky-glibc-ci.mk
@@ -4,15 +4,7 @@
 #
 ################################################################################
 
-<<<<<<< 2727efc6e78b7dee6079f4d85a7a54cd0b2eb38e
-<<<<<<< 84f68d58a151953c7281f01370f66538cea62e28
 CSKY_GLIBC_CI_VERSION = 3aab904d77f0c7e0d40a9fa565b372e0daab42d7
-=======
-CSKY_GLIBC_CI_VERSION = 9601e4bc9f6e35c1ca9420366618384038a205eb
->>>>>>> csky-glibc-ci: update sleep time & ip
-=======
-CSKY_GLIBC_CI_VERSION = 80594c8b3e2e44c824274e34637647a7a69cffff
->>>>>>> csky-glibc-ci: Fix ip
 CSKY_GLIBC_CI_SITE = $(call github,c-sky,csky-glibc-ci,$(CSKY_GLIBC_CI_VERSION))
 
 CSKY_GLIBC_CI_DEPENDENCIES = openssh
