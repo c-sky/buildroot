a9c16160e726a32657b9046b43a6d4755ac375f7
Liu Yibin <yibin_liu@c-sky.com>
2019-01-09 17:48:00 +0800

    Scripts can't handle failure cases
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- package/csky-ci/csky-ci.mk
+++ package/csky-ci/csky-ci.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################################
 
-CSKY_CI_VERSION = 52779beec3439ee414b99d0d6bca95911ce368e6
+CSKY_CI_VERSION = 434ddbc01d93f0b355fc7bfe0c41f18946750ef9
 CSKY_CI_SITE = $(call github,c-sky,csky-ci,$(CSKY_CI_VERSION))
 
 define CSKY_CI_CONFIGURE_CMDS
