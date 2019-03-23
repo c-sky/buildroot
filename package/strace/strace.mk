a8db5fabfd7ac7b929ce122fba4f1d6aefd86bc8
Guo Ren <ren_guo@c-sky.com>
2019-03-23 14:50:28 +0800

    package/strace: support csky strace
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-STRACE_VERSION = 4.20
+STRACE_VERSION = 4.26
 STRACE_SOURCE = strace-$(STRACE_VERSION).tar.xz
-STRACE_SITE = http://downloads.sourceforge.net/project/strace/strace/$(STRACE_VERSION)
-STRACE_LICENSE = BSD-3-Clause
-STRACE_LICENSE_FILES = COPYING
+STRACE_SITE = https://strace.io/files/$(STRACE_VERSION)
+STRACE_LICENSE = LGPL-2.1+
+STRACE_LICENSE_FILES = COPYING LGPL-2.1-or-later
+STRACE_CONF_OPTS = --enable-mpers=check
 
 # strace bundle some kernel headers to build libmpers, this mixes userspace
 # headers and kernel headers which break the build with musl.
@@ -18,10 +19,27 @@ STRACE_CONF_OPTS += st_cv_m32_mpers=no \
 	st_cv_mx32_mpers=no
 endif
 
+ifeq ($(BR2_PACKAGE_LIBUNWIND),y)
+STRACE_DEPENDENCIES += libunwind
+STRACE_CONF_OPTS += --with-libunwind
+else
+STRACE_CONF_OPTS += --without-libunwind
+endif
+
+# Demangling symbols in stack trace needs libunwind and libiberty.
+ifeq ($(BR2_PACKAGE_BINUTILS)$(BR2_PACKAGE_LIBUNWIND),yy)
+STRACE_DEPENDENCIES += binutils
+STRACE_CONF_OPTS += --with-libiberty=check
+else
+STRACE_CONF_OPTS += --without-libiberty
+endif
+
+ifeq ($(BR2_PACKAGE_PERL),)
 define STRACE_REMOVE_STRACE_GRAPH
 	rm -f $(TARGET_DIR)/usr/bin/strace-graph
 endef
 
 STRACE_POST_INSTALL_TARGET_HOOKS += STRACE_REMOVE_STRACE_GRAPH
+endif
 
 $(eval $(autotools-package))
