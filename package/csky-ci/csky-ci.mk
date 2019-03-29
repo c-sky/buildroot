4b2e660cf8ee2ba441285c9c8204897db38446ff
Liu Yibin <yibin_liu@c-sky.com>
2019-03-29 16:06:25 +0800

    Echo nothing when kill test.sh
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- package/csky-ci/csky-ci.mk
+++ package/csky-ci/csky-ci.mk
@@ -33,7 +33,7 @@ mkdir -p $(TARGET_DIR)/etc/init.ci/
 cp -f $(@D)/out/csky_* $(HOST_DIR)/csky-ci/
 cp -f $(@D)/out/S90test $(TARGET_DIR)/etc/init.d/
 cp -f $(@D)/out/test.sh $(TARGET_DIR)/etc/init.d/
-echo "killall test.sh" >> $(TARGET_DIR)/etc/profile
+echo "killall test.sh >> /dev/null 2>&1" >> $(TARGET_DIR)/etc/profile
 cp -f $(@D)/out/sh/* $(HOST_DIR)/csky-ci/
 endef
 
