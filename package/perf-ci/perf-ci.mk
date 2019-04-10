32c8287b6f3a82257b8e878b9c6d6c558bf5336d
Liu Yibin <yibin_liu@c-sky.com>
2019-04-10 11:02:56 +0800

    Run perf-kmem.sh in perf-ci
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- package/perf-ci/perf-ci.mk
+++ package/perf-ci/perf-ci.mk
@@ -8,6 +8,7 @@ define PERF_CI_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/perf-test
 	output/host/bin/csky-buildroot-*-gcc -O0 -g -fexceptions -mbacktrace package/perf-ci/callchain_test.c -o callchain_test
 	cp -f callchain_test $(TARGET_DIR)/usr/lib/perf-test
+	cp -f perf-kmem.sh $(TARGET_DIR)/usr/lib/perf-test
 	cp -f ./package/perf-ci/perf_run $(TARGET_DIR)/etc/init.ci/
 	chmod a+x $(TARGET_DIR)/etc/init.ci/perf_run
 	cp -f ./package/perf-ci/perf_parse $(HOST_DIR)/csky-ci/parse_script/
