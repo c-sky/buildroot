8b158968df1c1d3e7b4e223a286ba6174a9e39f9
Guo Ren <ren_guo@c-sky.com>
2019-10-06 21:19:11 +0800

    package/csky(riscv)-readme: Fixup qemu cmd
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/csky-readme/csky-readme.mk
+++ package/csky-readme/csky-readme.mk
@@ -9,6 +9,7 @@ CSKY_README_INSTALL_IMAGES = YES
 CSKY_README_BD_VERSION=$(shell git log --pretty=oneline | head -1 | awk '{print $$1}')
 CSKY_README_BD_CONFIG=$(shell grep BR2_DEFCONFIG $(CONFIG_DIR)/.config | awk -F/ '{print $$NF}'| sed 's/\"//g')
 CSKY_README_CK860=$(shell grep BR2_ck860=y $(CONFIG_DIR)/.config)
+CSKY_README_CK610=$(shell grep BR2_ck610=y $(CONFIG_DIR)/.config)
 
 define CSKY_README_INSTALL_IMAGES_CMDS
 	cp package/csky-readme/readme.txt $(BINARIES_DIR)/
@@ -17,7 +18,9 @@ define CSKY_README_INSTALL_IMAGES_CMDS
 	sed -i 's/<buildroot-version>/$(CSKY_README_BD_VERSION)/g' $(BINARIES_DIR)/readme.txt
 	sed -i 's/<kernel-version>/$(LINUX_VERSION)/g' $(BINARIES_DIR)/readme.txt
 	if [ -n "$(CSKY_README_CK860)" ]; then \
-		sed -i 's/qemu_start_cmd/LD_LIBRARY_PATH=.\/host\/lib .\/host\/csky-qemu\/bin\/qemu-system-cskyv2 -M virt -cpu ck860 -smp 2 -kernel vmlinux -nographic/g' $(BINARIES_DIR)/readme.txt; \
+		sed -i 's/qemu_start_cmd/LD_LIBRARY_PATH=.\/host\/lib .\/host\/csky-qemu\/bin\/qemu-system-cskyv2 -M virt -kernel vmlinux -nographic -smp 2/g' $(BINARIES_DIR)/readme.txt; \
+	elif [ -n "$(CSKY_README_CK610)" ]; then \
+		sed -i 's/qemu_start_cmd/LD_LIBRARY_PATH=.\/host\/lib .\/host\/csky-qemu\/bin\/qemu-system-cskyv1 -M virt -kernel vmlinux -nographic/g' $(BINARIES_DIR)/readme.txt; \
 	else \
 		sed -i 's/qemu_start_cmd/LD_LIBRARY_PATH=.\/host\/lib .\/host\/csky-qemu\/bin\/qemu-system-cskyv2 -M virt -kernel vmlinux -nographic/g' $(BINARIES_DIR)/readme.txt; \
 	fi; \
