22b1c96794f9e21ce33f44a9c520983d06708d9d
Guo Ren <ren_guo@c-sky.com>
2019-10-17 21:32:27 +0800

    package/hw-c860: Fixup boot error
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/hw-c860/hw/run.sh
+++ package/hw-c860/hw/run.sh
@@ -1,22 +1,15 @@
-if [ $# != 2 ]; then
-	echo "Usage: . run.sh <ip:port> <s/m>"
+if [ $# != 1 ]; then
+	echo "Usage: . run.sh <ip:port>"
 	return 0
 fi
 
-if [ $2 == 's' ]; then
-	dtc -I dts -O dtb fpga.c860s.dts.txt > hw.dtb
-	cp gdbinit.c860s.txt gdbinit -f
-elif [ $2 == 'm' ]; then
-	dtc -I dts -O dtb fpga.c860mp.dts.txt > hw.dtb
-	cp gdbinit.c860mp.txt gdbinit -f
-else
-	echo "No support"
-	return 0
-fi
+rm -f hw.dtb;
+rm -f gdbinit;
+dtc -I dts -O dtb fpga.c860mp.dts.txt > hw.dtb
+cp gdbinit.c860mp.txt gdbinit -f
 
 # Init DDR
-../../host/bin/csky-linux-gdb -ex "tar remote $1" -x ddrinit.txt ddr_init_elf
+./csky-linux-gdb -ex "tar remote $1" -x ddrinit.txt ddr_init_elf
 
 # Run linux
-../../host/bin/csky-linux-gdb -ex "tar remote $1" -x gdbinit
-
+./csky-linux-gdb -ex "tar remote $1" -x gdbinit -ex "c"
