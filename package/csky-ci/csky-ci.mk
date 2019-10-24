2a2503577b17a2df2f269de2bbb31e6910c8fc24
Mao Han <han_mao@c-sky.com>
2019-10-24 18:59:58 +0800

    .gitlab-ci.yml: Add chip test dh7200
    Signed-off-by: Mao Han <han_mao@c-sky.com>

--- package/csky-ci/csky-ci.mk
+++ package/csky-ci/csky-ci.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################################
 
-CSKY_CI_VERSION = 1c2e50bb885647daab80adf91370959814ce0ac4
+CSKY_CI_VERSION = 958d049a13f9b97d4fa4a62369584917cbf7cb2e
 CSKY_CI_SITE = $(call github,c-sky,csky-ci,$(CSKY_CI_VERSION))
 
 define CSKY_CI_CONFIGURE_CMDS
--- package/hw-c810/hw/run.sh
+++ package/hw-c810/hw/run.sh
@@ -3,6 +3,26 @@ if [ $# != 2 ]; then
 	return 0
 fi
 
+cd ..
+rm rootfs.cpio
+gzip -d rootfs.cpio.gz
+rm rootfs -rf
+mkdir -p rootfs
+cd rootfs
+cpio -idm < ../rootfs.cpio
+rm -f etc/init.d/S40network
+rm -f etc/init.d/S50sshd
+rm ../rootfs.cpio
+find . | cpio --quiet -o -H newc > ../rootfs.cpio
+cd ..
+gzip -c -9 rootfs.cpio >rootfs.cpio.gz
+cd hw
+ROOTFS_BASE=`cat $2.dts.txt |grep initrd-start|awk -F "<" '{print $2}'|awk -F ">" '{print $1}'`
+ROOTFS_SIZE=`ls -lt ../rootfs.cpio.gz|awk '{print $5}'`
+((ROOTFS_END= $ROOTFS_BASE + $ROOTFS_SIZE))
+ROOTFS_END=`printf "0x%x" $ROOTFS_END`
+sed -i "s/linux,initrd-end.*/linux,initrd-end = <$ROOTFS_END>;/g" $2.dts.txt
+
 if [ $2 == 'dh7200' ]; then
 	dtc -I dts -O dtb dh7200.dts.txt > hw.dtb
 	cp gdbinit.dh7200.txt gdbinit -f
@@ -20,5 +40,4 @@ if [ $2 == 'fpga' ]; then
 fi
 
 # Run linux
-../../host/bin/csky-linux-gdb -ex "tar remote $1" -x gdbinit -ex "c"
-
+../../host/bin/csky-linux-gdb -ex "tar remote $1" -x gdbinit -ex "c" -ex "q"
