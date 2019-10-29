6d0e638dea1f116697283caec93629879cf01082
Mao Han <han_mao@c-sky.com>
2019-10-29 18:08:35 +0800

    package/ntfs3g-ci: Add ntfs-3g ci test
    Signed-off-by: Mao Han <han_mao@c-sky.com>

---
+++ package/ntfs3g-ci/ntfs3g-ci.mk
@@ -0,0 +1,17 @@
+##############################################################################
+#
+# ntfs3g-ci
+#
+################################################################################
+
+define NTFS3G_CI_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/csky-ci/
+	cp -f package/ntfs3g-ci/ntfs3g_gz $(TARGET_DIR)/usr/lib/csky-ci/
+	chmod a+x $(TARGET_DIR)/usr/lib/csky-ci/ntfs3g_gz
+	cp -f package/ntfs3g-ci/ntfs3g_run $(TARGET_DIR)/etc/init.ci/
+	chmod a+x $(TARGET_DIR)/etc/init.ci/ntfs3g_run
+	cp -f package/ntfs3g-ci/ntfs3g_parse $(HOST_DIR)/csky-ci/parse_script/
+	chmod a+x $(HOST_DIR)/csky-ci/parse_script/ntfs3g_parse
+endef
+
+$(eval $(generic-package))
