5b6c825d4e1525919a839f3c4a80078e503ee7b4
guokai <yuanfei.gk@alibaba-inc.com>
2019-11-11 17:07:05 +0800

    package/csky-readme: Fixup linux-5.1 readme errors

--- package/csky-readme/csky-readme.mk
+++ package/csky-readme/csky-readme.mk
@@ -18,6 +18,9 @@ define CSKY_README_INSTALL_IMAGES_CMDS
 	sed -i 's/<buildroot-config>/$(CSKY_README_BD_CONFIG)/g' $(BINARIES_DIR)/readme.txt
 	sed -i 's/<buildroot-version>/$(CSKY_README_BD_VERSION)/g' $(BINARIES_DIR)/readme.txt
 	sed -i 's/<kernel-version>/$(LINUX_VERSION)/g' $(BINARIES_DIR)/readme.txt
+	if [ "5" == echo $(LINUX_VERSION) | awk -F"-" '{print $2}' | cut -d "." -f1 ]; then \
+		sed -i 's/cdn.kernel.org\/pub\/linux\/kernel\/v4.x/cdn.kernel.org\/pub\/linux\/kernel\/v5.x/g' $(BINARIES_DIR)/readme.txt;\
+	fi;\
 	if [ -n "$(CSKY_README_CK860)" ]; then \
 		sed -i 's/qemu_start_cmd/LD_LIBRARY_PATH=.\/host\/lib .\/host\/csky-qemu\/bin\/qemu-system-cskyv2 -M virt -kernel Image -nographic -append "console=ttyS0,115200 rdinit=\/sbin\/init rootwait root=\/dev\/vda ro" -drive file=rootfs.ext2,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -smp 4/g' $(BINARIES_DIR)/readme.txt; \
 		sed -i 's/linux_compile_cmd/LD_LIBRARY_PATH=..\/host\/lib make ARCH=csky CROSS_COMPILE=..\/host\/bin\/csky-linux- Image/g' $(BINARIES_DIR)/readme.txt; \
