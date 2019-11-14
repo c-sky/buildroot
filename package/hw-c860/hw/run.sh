fd04188daa82a099993815c8906c568ed3e0cd37
Guo Ren <ren_guo@c-sky.com>
2019-11-14 17:06:51 +0800

    package/csky-ci: Optimize fpga test
     - Cleanup riscv dts
     - Use Jtag -setclk 6 for all
     - Optimize ntfs3g_run script
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/hw-c860/hw/run.sh
+++ package/hw-c860/hw/run.sh
@@ -1,12 +1,14 @@
 echo "Usage: . run.sh <ip:port>"
 
-ROOTFS_BASE=`cat fpga.c860mp.dts.txt | grep initrd-start | awk -F "<" '{print $2}' | awk -F ">" '{print $1}'`
+cp fpga.c860mp.dts.txt .hw.dts
+
+ROOTFS_BASE=`cat .hw.dts | grep initrd-start | awk -F "<" '{print $2}' | awk -F ">" '{print $1}'`
 ROOTFS_SIZE=`ls -lt ../rootfs.cpio.gz | awk '{print $5}'`
 ((ROOTFS_END= $ROOTFS_BASE + $ROOTFS_SIZE))
 ROOTFS_END=`printf "0x%x" $ROOTFS_END`
-sed -i "s/linux,initrd-end.*/linux,initrd-end = <$ROOTFS_END>;/g" fpga.c860mp.dts.txt
+sed -i "s/linux,initrd-end.*/linux,initrd-end = <$ROOTFS_END>;/g" .hw.dts
 
-dtc -I dts -O dtb fpga.c860mp.dts.txt > hw.dtb
+dtc -I dts -O dtb .hw.dts > hw.dtb
 
 # Init DDR
 ./csky-linux-gdb -ex "tar remote $1" -x ddrinit.txt ddr_init_elf -ex "c" -ex "q"
--- package/hw-c910/hw/run.sh
+++ package/hw-c910/hw/run.sh
@@ -1,13 +1,12 @@
-if [ $# -lt 1 -o $# -gt 4 ] ; then
-        echo "Usage: . run.sh <ip:port> [an/eg] [s/m] [30/60]"
-        echo "Usage: [an/eg] is the board name"
-        echo "Usage: [s/m] is the number of cpu"
-        exit 0
+if [ $# -lt 1 -o $# -gt 3 ] ; then
+        echo "Usage: . run.sh <ip:port> [an/eg] [s/m]"
+        echo "Usage: [an/eg] is for board name"
+        echo "Usage: [s/m] is for onecore/twocore"
+        exit 1
 fi
 
 BOARD="eg"
 NRCORE="onecore"
-FREQ="60"
 
 for idx in "$@"
 do
@@ -20,31 +19,28 @@ do
                 NRCORE="onecore"
         elif [ $idx == "m" ]; then
                 NRCORE="twocore"
-        elif [ $idx == "30" ]; then
-                FREQ="30"
-        elif [ $idx == "60" ]; then
-                FREQ="60"
         fi
 done
 
 DDRINIT=ddrinit.$BOARD.txt
 GDBINIT=gdbinit.$BOARD.txt
-DTS=$NRCORE\_$BOARD\_$FREQ\M.dts.txt
-echo "Use config" $NRCORE $BOARD $FREQ
+DTS=$NRCORE\_$BOARD.dts.txt
+
+echo "Use config" $NRCORE $BOARD
 if [ ! -f $GDBINIT -o ! -f $DDRINIT -o ! -f $DTS ]; then
 	echo "No support"
 	exit 1
 fi
 
-cp $DTS .c960_lite.dts
+cp $DTS .hw.dts
 
-ROOTFS_BASE=`cat .c960_lite.dts | grep initrd-start | awk -F "<" '{print $2}' | awk -F ">" '{print $1}'`
+ROOTFS_BASE=`cat .hw.dts | grep initrd-start | awk -F "<" '{print $2}' | awk -F ">" '{print $1}'`
 ROOTFS_SIZE=`ls -lt ../rootfs.cpio.gz | awk '{print $5}'`
 ((ROOTFS_END= $ROOTFS_BASE + $ROOTFS_SIZE))
 ROOTFS_END=`printf "0x%x" $ROOTFS_END`
-sed -i "s/linux,initrd-end.*/linux,initrd-end = <$ROOTFS_END>;/g" .c960_lite.dts
+sed -i "s/linux,initrd-end.*/linux,initrd-end = <$ROOTFS_END>;/g" .hw.dts
 
-dtc -I dts -O dtb .c960_lite.dts > hw.dtb
+dtc -I dts -O dtb .hw.dts > hw.dtb
 
 # Init DDR
 ./riscv64-linux-gdb -ex "tar remote $1" -x $DDRINIT ddr_init_$BOARD\_elf -ex "c" -ex "q"
