dcd26bd283583039e4a5137aadde7ea0e39e7435
Guo Ren <ren_guo@c-sky.com>
2019-12-19 11:12:17 +0800

    package/hw-c860: Support more cores
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/hw-c860/hw/run.sh
+++ package/hw-c860/hw/run.sh
@@ -1,6 +1,55 @@
-echo "Usage: . run.sh <ip:port>"
+if [ $# -lt 1 -o $# -gt 3 ] ; then
+	echo "Usage: . run.sh <ip:port> [an/eg/ve] [1/2/3/4]"
+	echo "Usage: [an/eg/ve] is for platform"
+	echo "Usage: [1/2/3/4] is for cpu quantity"
+	exit 1
+fi
 
-cp fpga.c860mp.dts.txt .hw.dts
+BOARD="eg"
+NRCORE=2
+
+for idx in "$@"
+do
+if [ $idx == "an" ]; then
+	BOARD="an"
+elif [ $idx == "eg" ]; then
+	BOARD="eg"
+elif [ $idx == "ve" ]; then
+	BOARD="ve"
+elif [ $idx == "s" ]; then
+	NRCORE=1
+elif [ $idx == "m" ]; then
+	NRCORE=2
+fi
+case "$idx" in
+    [1-9] | [1-1][0-6])
+		NRCORE=$idx
+esac
+done
+
+set -ex
+
+DDRINIT=ddrinit.$BOARD.txt
+GDBINIT=gdbinit.$BOARD.txt
+DTS=$BOARD.dts.txt
+
+echo "Use config" $NRCORE $BOARD
+if [ ! -f $GDBINIT -o ! -f $DTS ]; then
+	echo "No support"
+	exit 1
+fi
+
+enable_dts_cores() {
+	let x=$1-1
+	for ((i=1;i<=$x;i++)); do
+		y=`awk '/cpu@'$i' \{/{getline a;print NR}' .hw.dts`
+		let y=$y+2
+		sed -i ''$y','$y's/fail/okay/g' .hw.dts
+	done
+}
+
+cp $DTS .hw.dts
+enable_dts_cores $NRCORE
 
 set -ex
 
@@ -13,7 +62,13 @@ sed -i "s/linux,initrd-end.*/linux,initrd-end = <$ROOTFS_END>;/g" .hw.dts
 dtc -I dts -O dtb .hw.dts > hw.dtb
 
 # Init DDR
-./csky-linux-gdb -ex "tar remote $1" -x ddrinit.txt ddr_init_elf -ex "c" -ex "q" > /dev/null
+if [ $BOARD == "ve" ]; then
+	echo "No need ddr_init for veloce"
+elif [ $BOARD == "eg" ]; then
+./csky-linux-gdb -ex "tar remote $1" -x $DDRINIT ddr_init_elf -ex "c" -ex "q" > /dev/null
+elif [ $BOARD == "an" ]; then
+./csky-linux-gdb -ex "tar remote $1" -x $DDRINIT -ex "q"
+fi
 
 # Run linux
-./csky-linux-gdb -ex "tar remote $1" -x gdbinit.c860mp.txt -ex "c" -ex "q"
+./csky-linux-gdb -ex "tar remote $1" -x $GDBINIT -ex "c" -ex "q"
