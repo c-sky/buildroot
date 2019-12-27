ddec1b53dae3b22aa644b7c5453bb9687d0585a8
Guo Ren <ren_guo@c-sky.com>
2019-12-27 14:02:25 +0800

    package/misc-download-c910: Fixup c910 connection failed.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- package/misc-download-c910/misc-download-c910.mk
+++ package/misc-download-c910/misc-download-c910.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MISC_DOWNLOAD_C910_VERSION = d55ec02c4ee9c5990313bb211256f0ee369cb1d5
+MISC_DOWNLOAD_C910_VERSION = e6f2523a2cda92bca08f40452a31cc44a17a9409
 MISC_DOWNLOAD_C910_SOURCE = misc-download-c910-$(MISC_DOWNLOAD_C910_VERSION).tar.gz
 MISC_DOWNLOAD_C910_SITE = $(call github,c-sky,910_misc_downloads,$(MISC_DOWNLOAD_C910_VERSION))
 
