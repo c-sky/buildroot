142258aa7163d8398ddc999bad8a29680f7ede46
Mao Han <han_mao@c-sky.com>
2019-12-31 11:12:57 +0800

    package/ntfs3g-ci: Optimize ntfs3g-ci logic and display
    Signed-off-by: Guo Kai <yuanfei.gk@alibaba-inc.com>
    Signed-off-by: Mao Han <han_mao@c-sky.com>

--- package/ntfs3g-ci/ntfs3g-ci.mk
+++ package/ntfs3g-ci/ntfs3g-ci.mk
@@ -6,8 +6,6 @@
 
 define NTFS3G_CI_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/csky-ci/
-	cp -f package/ntfs3g-ci/ntfs3g_gz $(TARGET_DIR)/usr/lib/csky-ci/
-	chmod a+x $(TARGET_DIR)/usr/lib/csky-ci/ntfs3g_gz
 	cp -f package/ntfs3g-ci/ntfs3g_run $(TARGET_DIR)/etc/init.ci/
 	chmod a+x $(TARGET_DIR)/etc/init.ci/ntfs3g_run
 	cp -f package/ntfs3g-ci/ntfs3g_parse $(HOST_DIR)/csky-ci/parse_script/
