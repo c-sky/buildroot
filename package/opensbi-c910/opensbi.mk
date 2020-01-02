6ee5ea1999cab8a62e9e33c71a28191245452d05
Liu Yibin <yibin_liu@c-sky.com>
2020-01-02 16:24:22 +0800

    package/opensbi-c910: Use opensbi from official repo to start
    From now on, opensbi-c910 will use opensbi's official
    repository along witch patches for pmu and medeleg.
    
    Signed-off-by: Liu Yibin <yibin_liu@c-sky.com>

--- package/opensbi-c910/opensbi.mk
+++ package/opensbi-c910/opensbi.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-OPENSBI_C910_VERSION = df3c95115e9add86e68fec8374f2ecf96d055125
+OPENSBI_C910_VERSION = c0849cd731fca884c939e6a1a699a853231292a2
 OPENSBI_C910_SOURCE = opensbi-c910-$(OPENSBI_C910_VERSION).tar.gz
-OPENSBI_C910_SITE = $(call github,c-sky,opensbi-c910,$(OPENSBI_C910_VERSION))
+OPENSBI_C910_SITE = $(call github,riscv,opensbi,$(OPENSBI_C910_VERSION))
 OPENSBI_C910_INSTALL_TARGET = NO
 OPENSBI_C910_INSTALL_STAGING = YES
 
@@ -22,7 +22,6 @@ define OPENSBI_C910_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(OPENSBI_C910_MAKE_ENV) \
 	FW_TEXT_START=0xc0000000 \
 	FW_JUMP_ADDR=0xc0200000 \
-	FW_JUMP_FDT_ADDR=0xd7000000 \
 	$(MAKE) -C $(@D)
 	mv $(@D)/build/platform/$(OPENSBI_C910_PLAT)/firmware/fw_jump.elf $(@D)/fw_jump_3G.elf
 	mv $(@D)/build/platform/$(OPENSBI_C910_PLAT)/firmware/fw_jump.bin $(@D)/fw_jump_3G.bin
@@ -33,7 +32,6 @@ define OPENSBI_C910_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(OPENSBI_C910_MAKE_ENV) \
 	FW_TEXT_START=0x00000000 \
 	FW_JUMP_ADDR=0x00200000 \
-	FW_JUMP_FDT_ADDR=0xd7000000 \
 	$(MAKE) -C $(@D)
 	mv $(@D)/build/platform/$(OPENSBI_C910_PLAT)/firmware/fw_jump.elf $(@D)/fw_jump_0G.elf
 	mv $(@D)/build/platform/$(OPENSBI_C910_PLAT)/firmware/fw_jump.bin $(@D)/fw_jump_0G.bin
