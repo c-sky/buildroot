37bbf15b3a5d5592136bd1c883a5895c9f7c4bc9
MaJun <mj145409@alibaba-inc.com>
2020-01-08 14:47:41 +0800

    package/hw-c810: Change the initrd setup method for 810 serial core
    Change the initrd setup method for 810 serial core just like c860 did
    
    Signed-off-by: MaJun <mj145409@alibaba-inc.com>

--- package/hw-c810/hw-c810.mk
+++ package/hw-c810/hw-c810.mk
@@ -9,6 +9,7 @@ HW_C810_INSTALL_IMAGES = YES
 define HW_C810_INSTALL_IMAGES_CMDS
 	mkdir -p $(BINARIES_DIR)/hw/
 	cp package/hw-c810/hw/* $(BINARIES_DIR)/hw/ -raf
+	cp $(BINARIES_DIR)/../host/bin/dtc $(BINARIES_DIR)/hw/ -raf
 endef
 
 $(eval $(generic-package))
---
+++ package/hw-c810/hw/setup_initrd.sh
@@ -0,0 +1,32 @@
+start_line=`grep -wnr "device_type" $1 | grep "memory" | awk -F ":" '{print $1}'`
+end_line=$(($start_line+2))
+
+#grep -A 5 -C 2 "device_type" .hw.dts | grep "memory" | awk -F ":" '{print $1}'
+echo "line numer is $start_line, $end_line"
+sed -n "$start_line,$end_line p" .hw.dts > memory.txt
+
+MEM_BASE=`cat memory.txt | grep -m 1 reg | awk -F "<" '{print $2}' | awk -F " " '{print $1}'`
+MEM_MASK=0xf0000000
+
+((MEM_BASE= $MEM_BASE & $MEM_MASK))
+MEM_BASE=`printf "0x%x" $MEM_BASE`
+
+ROOTFS_OFFSET=0x1000000
+((ROOTFS_BASE= $MEM_BASE + $ROOTFS_OFFSET))
+ROOTFS_BASE=`printf "0x%x" $ROOTFS_BASE`
+
+ROOTFS_SIZE=`ls -lt ../rootfs.cpio.gz | awk '{print $5}'`
+((ROOTFS_END= $ROOTFS_BASE + $ROOTFS_SIZE))
+ROOTFS_END=`printf "0x%x" $ROOTFS_END`
+
+
+echo "mem base is $MEM_BASE"
+echo "base is $ROOTFS_BASE"
+echo "size is $ROOTFS_SIZE"
+echo "end is $ROOTFS_END"
+
+sed -i '/initrd-start/d' $1
+sed -i '/initrd-end/d' $1
+
+sed -i "/bootargs/a\linux,initrd-start = <$ROOTFS_BASE>;" $1
+sed -i "/initrd-start/a\linux,initrd-end = <$ROOTFS_END>;" $1
