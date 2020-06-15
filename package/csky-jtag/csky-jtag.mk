24de1c1a4e2cfab6f25e2d6f225315975037dc2e
Ma Jun <majun258@linux.alibaba.com>
2020-06-15 16:41:17 +0800

    csky-jtag: Update the jtag version to 5.10
    To connect the anole board, the jtag tool need to be upgrade
    to 5.10 from 5.08.
    
    Signed-off-by: Ma Jun <majun258@linux.alibaba.com>

--- package/csky-jtag/csky-jtag.mk
+++ package/csky-jtag/csky-jtag.mk
@@ -4,11 +4,11 @@
 #
 ##############################################################################
 
-CSKY_JTAG_VERSION = CSKY-DebugServer-linux-x86_64-V5.8.13-20191211
+CSKY_JTAG_VERSION = CSKY-DebugServer-linux-x86_64-V5.10.4-20200429
 CSKY_JTAG_FILE = $(CSKY_JTAG_VERSION).sh
 CSKY_JTAG_SOURCE = $(CSKY_JTAG_FILE).tar.gz
-CSKY_JTAG_SITE = https://cop-image-prod.oss-cn-hangzhou.aliyuncs.com/resource/420262990181302272/1577331561983
-CSKY_JTAG_LINENUM = 276
+CSKY_JTAG_SITE = https://occ-oss-prod.oss-cn-hangzhou.aliyuncs.com/resource/1355977/1590483269667
+CSKY_JTAG_LINENUM = 277
 
 define CSKY_JTAG_EXTRACT_CMDS
 	tar -zxf $(CSKY_JTAG_DL_DIR)/$(CSKY_JTAG_SOURCE) -C $(@D)
