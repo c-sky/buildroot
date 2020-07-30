443dfc713ff5f0d963aad21d120c1e87a438f0d2
Liu Yibin <jiulong@linux.alibaba.com>
2020-07-30 16:03:20 +0800

    package/hw-c910: Add ppl & spl for ice evb as ddrinit
    Signed-off-by: Liu Yibin <jiulong@linux.alibaba.com>

--- package/misc-download-c910/misc-download-c910.mk
+++ package/misc-download-c910/misc-download-c910.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MISC_DOWNLOAD_C910_VERSION = f832bedd430d110f6de4acab6de8d07fd61fc231
+MISC_DOWNLOAD_C910_VERSION = 11c30bff06f6858e1e0970531bb50045ea05a108
 MISC_DOWNLOAD_C910_SOURCE = misc-download-c910-$(MISC_DOWNLOAD_C910_VERSION).tar.gz
 MISC_DOWNLOAD_C910_SITE = $(call github,c-sky,910_misc_downloads,$(MISC_DOWNLOAD_C910_VERSION))
 
