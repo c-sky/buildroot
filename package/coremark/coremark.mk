b8da4c7f972b2f1dab09b1613312a6a2a401f025
Guo Ren <guoren@linux.alibaba.com>
2020-12-10 17:27:25 +0800

    package/coremark: Fixup compile error with none enhanced toolchain
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>

--- package/coremark/coremark.mk
+++ package/coremark/coremark.mk
@@ -9,7 +9,11 @@ COREMARK_SITE = $(call github,eembc,coremark,v$(COREMARK_VERSION))
 COREMARK_LICENSE = Apache-2.0
 COREMARK_LICENSE_FILES = LICENSE.md
 
+ifeq ($(BR2_PACKAGE_HAS_TOOLCHAIN_EXTERNAL)$(BR2_riscv),yy)
 COREMARK_XCFLAGS="-O3 -static -funroll-all-loops -finline-limit=500 -fgcse-sm -fno-schedule-insns --param max-rtl-if-conversion-unpredictable-cost=100 -msignedness-cmpiv -fno-code-hoisting -mno-thread-jumps1 -mno-iv-adjust-addr-cost -mno-expand-split-imm"
+else
+COREMARK_XCFLAGS="-O2"
+endif
 
 define COREMARK_BUILD_CMDS
  $(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" XCFLAGS=$(COREMARK_XCFLAGS) -C $(@D) \
