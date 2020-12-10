fad6b747534c0de4620c4bb3226cf27f37bf0c70
Guo Ren <guoren@linux.alibaba.com>
2020-12-10 16:50:54 +0800

    package/coremark: Update XCFLAGS to support C910 performance
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>

--- package/coremark/coremark.mk
+++ package/coremark/coremark.mk
@@ -9,8 +9,10 @@ COREMARK_SITE = $(call github,eembc,coremark,v$(COREMARK_VERSION))
 COREMARK_LICENSE = Apache-2.0
 COREMARK_LICENSE_FILES = LICENSE.md
 
+COREMARK_XCFLAGS="-O3 -static -funroll-all-loops -finline-limit=500 -fgcse-sm -fno-schedule-insns --param max-rtl-if-conversion-unpredictable-cost=100 -msignedness-cmpiv -fno-code-hoisting -mno-thread-jumps1 -mno-iv-adjust-addr-cost -mno-expand-split-imm"
+
 define COREMARK_BUILD_CMDS
- $(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" -C $(@D) \
+ $(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" XCFLAGS=$(COREMARK_XCFLAGS) -C $(@D) \
  PORT_DIR=linux$(if $(BR2_ARCH_IS_64),64) EXE= link
 endef
 
